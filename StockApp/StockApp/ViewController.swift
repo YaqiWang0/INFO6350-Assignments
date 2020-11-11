62b672a18bc5b424ec50bf6588a2f5ea4eba36f6
Yaqi <wangyaqi9506@gmail.com>
2020-11-10 21:23:23 -0800

    update promise

--- StockApp/StockApp/ViewController.swift
+++ StockApp/StockApp/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 import SwiftSpinner
+import PromiseKit
 
 class ViewController: UIViewController {
 
@@ -23,27 +24,31 @@ class ViewController: UIViewController {
     @IBAction func GetValue(_ sender: Any) {
         guard let stockName = SymbolTXT.text else { return }
         let url = "\(apiURL)\(stockName)?apikey=\(apiKey)";
-        getStockValue(stockURL: url, stockName: stockName);
+        getStockValue(for : url, for : stockName).done { (ceo, price) in
+            self.ceo.text = "\(ceo)";
+            self.price.text = "$\(price)";
+        };
     }
     
-    func getStockValue(stockURL : String!, stockName : String!) {
-        SwiftSpinner.show("Getting Stock Value for \(String(describing: stockName))")
-        AF.request(stockURL)
-            .responseJSON { (response) in
-            SwiftSpinner.hide();
-            if response.error == nil {
-                guard let jsonString = response.data else {return}
-                
-                guard let stockJSON : [JSON] = JSON(jsonString).array else {return}
-                if stockJSON.count < 1 {return}
-                guard let symbol = stockJSON[0]["symbol"].rawString() else {return};
-                guard let price = stockJSON[0]["price"].double else {return};
-                guard let ceo = stockJSON[0]["ceo"].rawString() else {return};
-
-                print(price);
-                self.ceo.text = "\(ceo)";
-                self.price.text = "$\(price)";
-            }
+    func getStockValue(for stockURL : String!, for stockName : String!) -> Promise<(String, Double)>{
+        return Promise<(String, Double)> { seal -> Void in
+            SwiftSpinner.show("Getting Stock Value for \(String(describing: stockName))")
+                AF.request(stockURL)
+                    .responseJSON { (response) in
+                    SwiftSpinner.hide();
+                    if response.error == nil {
+                        guard let jsonString = response.data else {return}
+                        
+                        guard let stockJSON : [JSON] = JSON(jsonString).array else {return}
+                        if stockJSON.count < 1 {return}
+                        guard let symbol = stockJSON[0]["symbol"].rawString() else {return};
+                        guard let price = stockJSON[0]["price"].double else {return};
+                        guard let ceo = stockJSON[0]["ceo"].rawString() else {return};
+                        seal.fulfill((ceo, price))
+                    } else {
+                        seal.reject(response.error!)
+                    }
+                }
         }
     }
 }
